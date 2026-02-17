fad21a1c247c33dcc41c011dd75cd0962a9764ca
vanshikasharma6353-coder <vanshika.sharma6353@gmail.com>
2026-02-17 17:06:57 +0530

    Implement matrix addition in C

---
+++ 590028394-vanshika-011-Q1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    int mat1[100][100], mat2[100][100], result[100][100];
+    
+    scanf("%d %d", &m, &n);
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            result[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d", result[i][j]);
+            if (j < n - 1) printf(" ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
