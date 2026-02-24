dd9fd9496fed470f4fa258b55fbe5a7cba47e143
vanshikasharma6353-coder <vanshika.sharma6353@gmail.com>
2026-02-24 16:16:04 +0530

    Create 590028394-vanshika-021-Q1.c

---
+++ 590028394-vanshika-021-Q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d", head->data);
+        if (head->next != NULL) printf(" ");
+        head = head->next;
+    }
+    printf("\n");
+}
